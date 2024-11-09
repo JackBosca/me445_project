b10d5955782efcb154c82ccfaf3c9d428b39ff0b
Jacopo Boscariol <boscarioljacopo@gmail.com>
2024-11-09 23:57:15 +0100

    added plots submodule and tail cl curve and derived airfoil

--- main.m
+++ main.m
@@ -16,21 +16,16 @@ clear
 close all
 clc
 
-% config call
+%% config call
 run("config.m")
 
-% plot NACA 23012
-figure
-plot(x, y, '-o', 'LineWidth', 1.5, 'MarkerSize', 3, 'MarkerFaceColor', 'b')
-axis equal
-xlabel('x/c')
-ylabel('y/c')
-grid on
-
-%% Searching an optimal geometrical Joukowski transformation
+%% Optimal geometrical Joukowski transformation
 
 % initial guess for parameters eta_origin, xi_origin, a
 init_guess = [-0.1, 0.1, 1];
+
+% error definition (to be specified in objective_geom)
+% err_type_geom = 'sum_squared';
 
 % minimize the objective function
 opt_params_geom = fminsearch(@(params) objective_geom(params, x, y), ...
@@ -39,68 +34,47 @@ opt_params_geom = fminsearch(@(params) objective_geom(params, x, y), ...
 % Joukowski function call with optimal params
 [~, xj, yj] = joukowski_transform(opt_params_geom);
 
-yj_interp = profile_interpolator(xj, yj, x);
+yj_interp_geom = profile_interpolator(xj, yj, x);
 
-% add Joukowski plot to the NACA23012 one
-hold on
-plot(x, yj_interp, 'Marker', 'p')
+%% Optimal cl Joukowski transformation
 
-%% Searching an optimal cl Joukowski transformation
+% stall angles
+alphas_p = 7;                           % (deg)
+alphas_n = -8;                          % (deg)
 
-% search linear interval in cl(alpha) curve
-idx1 = find(fig8_clW(:, 1) > 0, 1);
-idx2 = find(fig8_clW(:, 1) > 7, 1);
-alpha = deg2rad(fig8_clW(idx1:idx2, 1));
-cl = fig8_clW(idx1:idx2, 2);
+% search linear interval in clW(alpha) curve
+idx1 = find(fig8_clW(:, 1) > alphas_n, 1);
+idx2 = find(fig8_clW(:, 1) > alphas_p, 1);
+alphaW = deg2rad(fig8_clW(idx1:idx2, 1));
+clW = fig8_clW(idx1:idx2, 2);
+
+% search linear interval in clT(alpha) curve
+idx1 = find(fig8_clT(:, 1) > alphas_n, 1);
+idx2 = find(fig8_clT(:, 1) > alphas_p, 1);
+alphaT = deg2rad(fig8_clT(idx1:idx2, 1));
+clT = fig8_clT(idx1:idx2, 2);
+
+% error definition (to be specified in objective_geom)
+% err_type_cl = 'sum_squared';
 
 % minimize the objective function
-opt_params_cl = fminsearch(@(params) objective_cl(params, alpha, cl), ...
+opt_params_clW = fminsearch(@(params) objective_cl(params, alphaW, clW), ...
+    init_guess);
+opt_params_clT = fminsearch(@(params) objective_cl(params, alphaT, clT), ...
     init_guess);
 
 % Joukowski function call with optimal params
-[~, xj, yj] = joukowski_transform(opt_params_cl);
+[~, xjW, yjW] = joukowski_transform(opt_params_clW);
+[~, xjT, yjT] = joukowski_transform(opt_params_clT);
 
-yj_interp = profile_interpolator(xj, yj, x);
+yj_interp_clW = profile_interpolator(xjW, yjW, x);
+yj_interp_clT = profile_interpolator(xjT, yjT, x);
 
-% add Joukowski plot to the NACA23012 one
-plot(x, yj_interp, 'Marker', 'p')
-
-% dynamic legend
-if ~exist('err_type_geom', 'var')
-    err_type_geom = 'mean-abs';
-end
-lgd_text_geom = sprintf('Joukowski Airfoil - Geometry Opt by %s', ...
-    err_type_geom);
-lgd_text_cl = sprintf('Joukowski Airfoil - Cl Opt by %s', ...
-    err_type_geom);
-legend('NACA 23012', lgd_text_geom, lgd_text_cl)
-
-%% wing cl
-
-figure
-plot(fig8_clW(:, 1), fig8_clW(:, 2), 'Marker', 'p')
-hold on
-plot(fig8_clT(:, 1), fig8_clT(:, 2), 'Marker', 'p')
-grid on
-xlabel('\alpha (deg)')
-
+% cl potential flow function
 cl = @(alpha, lambda, a) 2*pi*(alpha + lambda/a);
 
-alpha = deg2rad(0:1:7);
+% linear alphas interval
+alpha = deg2rad(alphas_n:1:alphas_p);
 
-plot(rad2deg(alpha), cl(alpha, opt_params_geom(2), opt_params_geom(3)), ...
-    'Marker', 'x')
-plot(rad2deg(alpha), cl(alpha, opt_params_cl(2), opt_params_cl(3)), ...
-    'Marker', 'x')
-
-% dynamic legend
-if ~exist('err_type_cl', 'var')
-    err_type_cl = 'mean-abs';
-end
-lgd_text_geom = sprintf('Joukowski Airfoil - Geometry Opt by %s', ...
-    err_type_cl);
-lgd_text_cl = sprintf('Joukowski Airfoil - Cl Opt by %s', ...
-    err_type_cl);
-
-legend('NACA 23012 cl_W', 'NACA 23012 cl_T', lgd_text_geom, lgd_text_cl)
-title('Fig. 8')
+%% plots call
+run("plots.m")
---
+++ plots.m
@@ -0,0 +1,80 @@
+%{
+plots.m - Plots script for the Aerodynamics ME-445 project. Selected paper: 
+https://doi.org/10.1007/s00348-017-2429-4.
+
+CALLED FUNCTIONS: -
+
+CALLED DATA FILES: -
+
+REVISIONS:
+- #v0, 09-11-2024, Release, Boscariol Jacopo
+
+Changes: -
+%}
+
+% set colors
+col_geom = 'blue';
+colW = 'red';
+colT = 'green';
+
+%% Airfoils
+% plot NACA 23012
+figure
+plot(x, y, '-o', 'LineWidth', 1.5, 'MarkerSize', 3, ...
+    'MarkerFaceColor', 'black', 'Color', 'black')
+axis equal
+xlabel('x/c')
+ylabel('y/c')
+grid on
+
+% add opt geometrical Joukowski plot
+hold on
+plot(x, yj_interp_geom, 'Marker', 'p', 'Color', col_geom)
+
+% add opt cl Joukowski plot
+plot(x, yj_interp_clW, 'Marker', 'p', 'Color', colW)
+plot(x, yj_interp_clT, 'Marker', 'p', 'Color', colT)
+
+% dynamic legend
+if ~exist('err_type_geom', 'var')
+    err_type_geom = 'mean-abs';
+end
+lgd_text_geom = sprintf('Joukowski Airfoil - Geometry Opt by %s', ...
+    err_type_geom);
+lgd_text_clW = sprintf('Joukowski Wing Airfoil - cl Opt by %s', ...
+    err_type_geom);
+lgd_text_clT = sprintf('Joukowski Tail Airfoil - cl Opt by %s', ...
+    err_type_geom);
+
+legend('NACA 23012', lgd_text_geom, lgd_text_clW, lgd_text_clT)
+
+%% cl curves
+
+figure
+plot(fig8_clW(:, 1), fig8_clW(:, 2), 'Marker', 'p', 'Color', 'black')
+hold on
+plot(fig8_clT(:, 1), fig8_clT(:, 2), 'Marker', 'square', 'Color', 'black')
+grid on
+xlabel('\alpha (deg)')
+
+plot(rad2deg(alpha), cl(alpha, opt_params_geom(2), opt_params_geom(3)), ...
+    'Marker', 'x', 'Color', col_geom)
+plot(rad2deg(alpha), cl(alpha, opt_params_clW(2), opt_params_clW(3)), ...
+    'Marker', 'x', 'Color', colW)
+plot(rad2deg(alpha), cl(alpha, opt_params_clT(2), opt_params_clT(3)), ...
+    'Marker', 'x', 'Color', colT)
+
+% dynamic legend
+if ~exist('err_type_cl', 'var')
+    err_type_cl = 'mean-abs';
+end
+lgd_text_geom = sprintf('Joukowski Airfoil - Geometry Opt by %s', ...
+    err_type_cl);
+lgd_text_clW = sprintf('Joukowski Wing Airfoil - cl Opt by %s', ...
+    err_type_geom);
+lgd_text_clT = sprintf('Joukowski Tail Airfoil - cl Opt by %s', ...
+    err_type_geom);
+
+legend('NACA 23012 cl_W', 'NACA 23012 cl_T', lgd_text_geom, ...
+    lgd_text_clW, lgd_text_clT)
+title('Fig. 8')
